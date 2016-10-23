b76bd3b96ebc1ca6100ced74c745132ae8996715
Luke Jakimowicz <luke@evidentagenda.com>
2016-10-23 13:53:38 +0200

    Initial commit

---
+++ dist/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, user-scalable=no,initial-scale=1,maximum-scale=1"/>
+	<meta name="description" content=""/>
+	<meta name="keywords" content=""/>
+
+	<title>Document</title>
+	
+	<link rel="stylesheet" href="css/foundation.css">
+	<link rel="stylesheet" href="css/style.css">
+	
+</head>
+<body>
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	<!-- vendor scripts -->
+	<script src="js/vendors.js"></script>
+	<!-- end of vendor scripts -->
+
+	<script src="js/ea.scripts.js"></script>
+
+	<!-- livereload script - dev env only -->
+	<script src="//<?php echo $_SERVER['SERVER_NAME']; ?>:35729/livereload.js"></script>
+
+</body>
+</html>
